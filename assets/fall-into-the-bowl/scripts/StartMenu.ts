6faca3b83eca364a274e1e94be37f2cd5944ab44
lili.nuyoah <lili.nuyoah@bytedance.com>
2026-01-19 16:46:06 +0800

    feat: 🎸 添加基础的ui类和startMenu空节点

---
+++ assets/fall-into-the-bowl/scripts/StartMenu.ts
@@ -0,0 +1,15 @@
+import { _decorator, Component, Node } from 'cc';
+import { UIBase } from './UIBase';
+const { ccclass, property } = _decorator;
+
+@ccclass('StartMenu')
+export class StartMenu extends UIBase {
+    start() {
+
+    }
+
+    update(deltaTime: number) {
+        
+    }
+}
+
