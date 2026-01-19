a4086e63594a4cfdebd08320e54ca28a542a11ee
lili.nuyoah <lili.nuyoah@bytedance.com>
2026-01-19 17:09:10 +0800

    feat: 🎸 完成开始菜单的初始化

--- assets/fall-into-the-bowl/scripts/StartMenu.ts
+++ assets/fall-into-the-bowl/scripts/StartMenu.ts
@@ -1,15 +1,23 @@
-import { _decorator, Component, Node } from 'cc';
+import { _decorator, Component, Node, tween } from 'cc';
 import { UIBase } from './UIBase';
 const { ccclass, property } = _decorator;
 
 @ccclass('StartMenu')
 export class StartMenu extends UIBase {
-    start() {
+    @property({ type: Node, displayName: '开始游戏按钮' })
+    startGameButton: Node | null = null;
 
+    @property({ type: Node, displayName: '关卡选择按钮' })
+    levelSelectButton: Node | null = null;
+
+    protected onLoad(): void {
+        super.onLoad();
+        this.rotateStartButtonIcon();
     }
 
-    update(deltaTime: number) {
-        
+    private rotateStartButtonIcon() {
+        const node = this.startGameButton?.children?.[0]
+        node && tween(node).repeatForever(tween().to(0.5, { angle: 10 }).to(0.5, { angle: 0 })).start()
     }
 }
 
--- assets/fall-into-the-bowl/scripts/UIManager.ts
+++ assets/fall-into-the-bowl/scripts/UIManager.ts
@@ -1,11 +1,16 @@
-import { _decorator, Component, Node } from 'cc';
+import { _decorator, Component, instantiate, Node, Prefab } from 'cc';
 import { StaticSingleton } from './StaticSingleton';
 const { ccclass, property } = _decorator;
 
 @ccclass('UIManager')
 export class UIManager extends Component {
+    @property(Prefab)
+    startMenuPrefab: Prefab | null = null;
+
     protected onLoad(): void {
         StaticSingleton.setUIManager(this);
+        const startMenuNode = instantiate(this.startMenuPrefab);
+        startMenuNode.parent = this.node;
     }
 
     start() {
