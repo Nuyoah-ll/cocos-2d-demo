63dae01150e38683bcf1f8ab1fc4f3ddf5d2f16d
cherrypick <1120182356@qq.com>
2026-01-23 23:35:12 +0800

    feat: 完成事件中心

--- assets/games/cramped-room-of-death/scripts/runtime/DataManager.ts
+++ assets/games/cramped-room-of-death/scripts/runtime/DataManager.ts
@@ -2,6 +2,7 @@ import { _decorator, Component, Node } from 'cc';
 import { ITile } from '../levels';
 const { ccclass, property } = _decorator;
 
+// 全局数据管理
 export class DataManager {
     static mapInfo: Array<Array<ITile>>
     static mapRowCount: number
